84e49024f85601c0a66b72579ca9437da444be5f
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-02-10 18:23:44 +0100

    Created 'combined operators' operator

---
+++ src/classes/combined-operators.ts
@@ -0,0 +1,32 @@
+import { Vector } from "p5";
+import { GenericOperator } from "./generic-operators";
+import { Input } from "./input";
+import { Output } from "./output";
+
+export class CombinedOperators extends GenericOperator {
+  inputOperators: Input[];
+  outputOperators: Output[];
+
+  constructor(pos: Vector, operators: GenericOperator[], name: string) {
+    // Extract the inputs and outputs from all the operators
+    // And the sort them in order of their pos.y component
+    // This is so that the input/output nodes will match up with the internal input/output operators
+    const inputOperators = operators.filter(op => op instanceof Input).sort((a, b) => b.pos.y - a.pos.y) as Input[];
+    const outputOperators = operators.filter(op => op instanceof Output).sort((a, b) => b.pos.y - a.pos.y) as Output[];
+
+    // Pass the number of inputs and outputs to the GenericOperator
+    // So that it can create the appopriate amount of nodes
+    super(pos, inputOperators.length, outputOperators.length, name);
+
+    this.inputOperators = inputOperators;
+    this.outputOperators = outputOperators;
+  }
+
+  logic(): void {
+    // Loop over every input node, and set every internal input operator to the state
+    this.inputs.forEach((inp, idx) => this.inputOperators[idx].state = inp.status);
+
+    // Loop over every output node, and set it's state the match the internal output opetators state
+    this.outputs.forEach((out, idx) => out.setStatus(this.outputOperators[idx].state));
+  }
+}
